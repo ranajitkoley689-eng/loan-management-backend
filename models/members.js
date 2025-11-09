a28f97f2db6291e9cb1593b81eae94d5746ed811
ranajitkoley689-eng <ranajitkoley689@gmail.com>
2025-11-09 23:31:31 +0530

    Update and rename Member.js to members.js

--- models/members.js
+++ models/members.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   aadhaar: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
-  contact: { type: String, required: true },
-  loanDate: { type: Date, required: true },
   group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
-  loans: [{ type: mongoose.Schema.Types.ObjectId, ref: "Loan" }]
+  loans: [{ type: mongoose.Schema.Types.ObjectId, ref: "Loan" }],
 }, { timestamps: true });
 
-export default mongoose.model("Member", memberSchema);
+const Member = mongoose.model("Member", memberSchema);
+export default Member;
