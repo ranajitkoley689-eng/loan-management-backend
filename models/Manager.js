fde496372aa17d340d4f0925d2c177d6b5007a84
ranajitkoley689-eng <ranajitkoley689@gmail.com>
2025-11-09 23:29:56 +0530

    Refactor manager schema to include name and email

--- models/Manager.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const managerSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // hashed password
-}, { timestamps: true });
-
-export default mongoose.model("Manager", managerSchema);
---
+++ models/manager.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const managerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+const Manager = mongoose.model("Manager", managerSchema);
+export default Manager;
