0caae1034d0191ef7b07ee7eff91e5c3bba3cc6a
ranajitkoley689-eng <ranajitkoley689@gmail.com>
2025-11-09 23:28:40 +0530

    Refactor Worker model export syntax

--- models/Worker.js
+++ models/Worker.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const workerSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }]
+  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
 }, { timestamps: true });
 
-export default mongoose.model("Worker", workerSchema);
+const Worker = mongoose.model("Worker", workerSchema);
+export default Worker;
