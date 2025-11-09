6a6c5eef7ff429bb7da75cfc3945b37d87a9eb71
ranajitkoley689-eng <ranajitkoley689@gmail.com>
2025-11-09 21:11:59 +0530

    Update password field comment in Manager schema

--- models/Manager.js
+++ models/Manager.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const managerSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true } // hashed with bcrypt
+  password: { type: String, required: true } // hashed password
 }, { timestamps: true });
 
 export default mongoose.model("Manager", managerSchema);
